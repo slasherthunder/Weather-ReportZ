297b6a4f96abdd8f96f0e29b296cbc707fb0d481
Jer0st <164413660+Jer0st@users.noreply.github.com>
2025-05-17 14:57:01 -0700

    Update MainActivity.kt

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -1,20 +1,96 @@
 package com.example.weather
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import android.widget.Button
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textViewWeather: TextView
+    private val editTextLocation: EditText? = null
+    private lateinit var apiService: WeatherApiService
+
+    private val APIKEY = "bd19138f2a75421f88535220251705"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //Storm text
+        val test = findViewById<TextView>(R.id.descriptionText)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.weatherapi.com/v1/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        apiService = retrofit.create(WeatherApiService::class.java)
+
+        //Button that changes city
+        val changeButton = findViewById<Button>(R.id.fetchWeatherButton)
+        //When button is clicked
+        changeButton.setOnClickListener {
+            //Varible for the inputted location
+            val region: String = findViewById<TextView>(R.id.cityNameInput).text.toString().trim()
+            if (!region.isEmpty()){
+                fetchWeather(region)
+            }
+
+
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    fun fetchWeather(place: String){
+        val call  = apiService.getCurrentWeather(APIKEY, place)
+        call.enqueue(object : Callback<WeatherResponse> {
+            override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
+                if (response.isSuccessful) {
+                    val weatherData = response.body()
+                    // Access your data here
+                    val name = weatherData?.location?.name
+                    val tempature = weatherData?.current?.temp_f
+                    Log.d("FindMe", tempature.toString())
+                    val textTest = findViewById<TextView>(R.id.temperatureText)
+                    textTest.text = tempature.toString()
+                    val cityTest = findViewById<TextView>(R.id.cityNameText)
+                    cityTest.text = name.toString()
+
+
+                    // Update your UI with the data
+                } else {
+                    // Handle error
+                    Log.e("API", "Error: ${response.code()}")
+                }
+            }
+
+            override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
+                // Handle failure
+                Log.e("API", "Failure: ${t.message}")
+            }
+        })
+
+    }
+
+
 }
